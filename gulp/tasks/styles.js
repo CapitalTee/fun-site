6634d69662627973d3e6427b40de74ed499b4de8
Martina Schiele <mschiele@gruender-sites.com>
2016-11-24 08:45:33 +0100

    Completed large hero block and intro content block.

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,20 @@
+var gulp = require ('gulp'),
+    postcss = require ('gulp-postcss'),
+    nested = require('postcss-nested'),
+    simplevars = require ('postcss-simple-vars'),
+    cssImport = require ('postcss-import'),
+    autoprefixer = require ('autoprefixer'),
+    mixins = require('postcss-mixins');
+    
+gulp.task('styles', function() {
+	return gulp.src('./app/assets/styles/styles.css')
+	
+	.pipe(postcss([cssImport, mixins, simplevars, nested, autoprefixer]))
+        .on('error',function(errorInfo) {
+            console.log(errorInfo.toString());
+            this.emit('end');
+        })
+	.pipe(gulp.dest('./app/temp/styles'));
+});    
+        
+
